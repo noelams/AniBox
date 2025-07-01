0705f0dc99dfea082d4cf55b7911e6be85ed6202
Ali Noel <noelams03@gmail.com>
2025-07-01 15:44:25 +0100

    MVP backend completed

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Acess denied. No Token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Invalid token", error);
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
+
+module.exports = verifyToken;
---
+++ Backend/routes/animeLogRoutes.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const AnimeLog = require("../models/AnimeLog");
+const verifyToken = require("../middleware/authMiddleware");
+
+router.post("/", verifyToken, async (req, res) => {
+  const { animeId, status, review, score } = req.body;
+
+  if (!animeId || !status) {
+    return res.status(400).json({ message: "animeId and status are required" });
+  }
+  try {
+    const log = new AnimeLog({
+      userId: req.user._id,
+      animeId,
+      status,
+      review,
+      score,
+    });
+    const savedLog = await log.save();
+    res.status(201).json(savedLog);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+});
+
+router.get("/", verifyToken, async (req, res) => {
+  const { status, animeId } = req.query;
+
+  try {
+    const query = { userId: req.user._id };
+    if (status) query.status = status;
+    if (animeId) query.animeId = animeId;
+    const logs = await AnimeLog.find(query).sort({ createdAt: -1 });
+    res.json(logs);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error fetching logs", error: err.message });
+  }
+});
+
+router.put("/:id", verifyToken, async (req, res) => {
+  try {
+    const updated = await AnimeLog.findOneAndUpdate(
+      { _id: req.params.id, userId: req.user._id },
+      req.body,
+      { new: true }
+    );
+    if (!updated)
+      return res
+        .status(404)
+        .json({ message: "Log not found or not authorized" });
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({
+      mesage: "Error updating log",
+      error: err.message,
+    });
+  }
+});
+
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    const deleted = await AnimeLog.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.user._id,
+    });
+
+    if (!deleted)
+      return res
+        .status(404)
+        .json({ message: "Log not found or not authorized" });
+
+    res.json({ message: "Anime log deleted", deleted });
+  } catch (err) {
+    res.status(500).json({ message: "Error deleting log", error: err.message });
+  }
+});
+
+module.exports = router;
